7628ecc5db65551ac97b3af88fabf57d78faa3b1
Alber Derry <alberdr1912@gmail.com>
2024-06-07 19:28:01 +0800

    add funct penilaiannn

--- lib/penilaian_screen.dart
+++ lib/penilaian_screen.dart
@@ -17,6 +17,7 @@ class _PenilaianScreenState extends State<PenilaianScreen> {
   final DatabaseHelper _dbHelper = DatabaseHelper();
   final List<TextEditingController> sebelumControllers = [];
   final List<TextEditingController> sesudahControllers = [];
+  final List<TextEditingController> keteranganControllers = []; // Tambahkan controller untuk keterangan
   List<Map<String, dynamic>> data = [];
   List<Map<String, dynamic>> existingEntries = [];
   double totalSkor = 0;
@@ -37,6 +38,7 @@ class _PenilaianScreenState extends State<PenilaianScreen> {
       for (var i = 0; i < data.length; i++) {
         sebelumControllers.add(TextEditingController());
         sesudahControllers.add(TextEditingController());
+        keteranganControllers.add(TextEditingController()); // Tambahkan controller untuk setiap item
       }
     });
   }
@@ -51,6 +53,7 @@ class _PenilaianScreenState extends State<PenilaianScreen> {
             if (entry['sub_indikator'] == data[i]['sub_indikator']) {
               sebelumControllers[i].text = entry['sebelum'] ?? '';
               sesudahControllers[i].text = entry['sesudah'] ?? '';
+              keteranganControllers[i].text = entry['keterangan'] ?? ''; // Set text untuk keterangan
               data[i]['entry_id'] = entry['entry_id'].toString();  // Ensure entry_id is stored as String
             }
           }
@@ -59,7 +62,6 @@ class _PenilaianScreenState extends State<PenilaianScreen> {
       });
     }
   }
-
 
   void _calculateTotalScore() {
     bool allSesudahFilled = true;
@@ -80,45 +82,44 @@ class _PenilaianScreenState extends State<PenilaianScreen> {
       }
     });
   }
-Future<void> _saveDataEntry() async {
-  // Dapatkan daftar kegiatan untuk user
-  List<Map<String, dynamic>> kegiatanList = await _dbHelper.getKegiatanForUser(widget.userId);
 
-  // Temukan kegiatan yang sesuai dengan kegiatanId yang diberikan
-  Map<String, dynamic> kegiatan = kegiatanList.firstWhere(
-    (kegiatan) => kegiatan['kegiatan_id'] == widget.kegiatanId,
-    orElse: () => <String, dynamic>{},
-  );
+  Future<void> _saveDataEntry() async {
+    // Dapatkan daftar kegiatan untuk user
+    List<Map<String, dynamic>> kegiatanList = await _dbHelper.getKegiatanForUser(widget.userId);
 
-  // Ambil nilai nama_puskesmas dari kegiatan
-  String puskesmas = kegiatan.isNotEmpty ? kegiatan['nama_puskesmas'] : '';
+    // Temukan kegiatan yang sesuai dengan kegiatanId yang diberikan
+    Map<String, dynamic> kegiatan = kegiatanList.firstWhere(
+      (kegiatan) => kegiatan['kegiatan_id'] == widget.kegiatanId,
+      orElse: () => <String, dynamic>{},
+    );
 
-  for (var i = 0; i < data.length; i++) {
-    Map<String, dynamic> entry = {
-      'user_id': widget.userId,
-      'kegiatan_id': widget.kegiatanId,
-      'id_category': widget.id_category,
-      'puskesmas': puskesmas,
-      'indikator': data[i]['nama_indikator'],
-      'sub_indikator': data[i]['sub_indikator'],
-      'kriteria': data[i]['kriteria'] ?? '',
-      'sebelum': sebelumControllers[i].text,
-      'sesudah': sesudahControllers[i].text,
-      'keterangan': data[i]['keterangan'] ?? '',
-    };
+    // Ambil nilai nama_puskesmas dari kegiatan
+    String puskesmas = kegiatan.isNotEmpty ? kegiatan['nama_puskesmas'] : '';
 
-    if (data[i].containsKey('entry_id')) {
-      entry['entry_id'] = int.parse(data[i]['entry_id']);
+    for (var i = 0; i < data.length; i++) {
+      Map<String, dynamic> entry = {
+        'user_id': widget.userId,
+        'kegiatan_id': widget.kegiatanId,
+        'id_category': widget.id_category,
+        'puskesmas': puskesmas,
+        'indikator': data[i]['nama_indikator'],
+        'sub_indikator': data[i]['sub_indikator'],
+        'kriteria': '',
+        'sebelum': sebelumControllers[i].text,
+        'sesudah': sesudahControllers[i].text,
+        'keterangan': keteranganControllers[i].text, // Simpan keterangan
+      };
+
+      if (data[i].containsKey('entry_id')) {
+        entry['entry_id'] = int.parse(data[i]['entry_id']);
+      }
+
+      await _dbHelper.saveDataEntry(entry);
     }
 
-    await _dbHelper.saveDataEntry(entry);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data berhasil disimpan')));
+    Navigator.pop(context);
   }
-
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data berhasil disimpan')));
-  Navigator.pop(context);
-}
-
-
 
   void _showPopup(BuildContext context, String content) {
     showDialog(
@@ -148,148 +149,155 @@ Future<void> _saveDataEntry() async {
     for (var controller in sesudahControllers) {
       controller.dispose();
     }
+    for (var controller in keteranganControllers) { // Dispose keterangan controllers
+      controller.dispose();
+    }
     super.dispose();
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Row(
-        children: [
-          Text('Penilaian'),
-          SizedBox(width: 10),
-          if (totalSkor > 0) ...[
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 11, vertical: 5), // Sesuaikan padding
-              margin: EdgeInsets.fromLTRB(70, 0, 0, 0),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 144, 190, 228),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'Score: ${totalSkor.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 14), // Sesuaikan fontSize
-              ),
-            ),
-          ],
-        ],
-      ),
-    ),
-    body: data.isEmpty
-        ? Center(child: CircularProgressIndicator())
-        : ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return Card(
-                margin: EdgeInsets.all(10),
-                shape: RoundedRectangleBorder(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text('Penilaian'),
+            SizedBox(width: 10),
+            if (totalSkor > 0) ...[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 11, vertical: 5), // Sesuaikan padding
+                margin: EdgeInsets.fromLTRB(70, 0, 0, 0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 144, 190, 228),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.asset(
-                              'assets/images/logors.jpg', // Ganti dengan path gambar Anda
-                              width: 50,
-                              height: 50,
-                              fit: BoxFit.cover,
+                child: Text(
+                  'Score: ${totalSkor.toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 14), // Sesuaikan fontSize
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+      body: data.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.asset(
+                                'assets/images/logors.jpg', // Ganti dengan path gambar Anda
+                                width: 50,
+                                height: 50,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          SizedBox(width: 10),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data[index]["nama_indikator"] ?? '',
-                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  data[index]["sub_indikator"] ?? '',
-                                  style: TextStyle(fontSize: 14, color: Colors.grey),
-                                ),
-                                // Text(
-                                //   data[index]["kriteria"] ?? '',
-                                //   style: TextStyle(fontSize: 14, color: Colors.grey),
-                                // ),
-                              ],
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              Row(
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.visibility,
-                                      color: Colors.blue,
-                                    ),
-                                    onPressed: () async {
-                                      _showPopup(context, data[index]["keterangan"] ?? '');
-                                    },
+                                  Text(
+                                    data[index]["nama_indikator"] ?? '',
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                                   ),
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.help_outline,
-                                      color: Colors.orange,
-                                    ),
-                                    onPressed: () async {
-                                      _showPopup(context, data[index]["kriteria"] ?? '');
-
-                                    },
+                                  Text(
+                                    data[index]["sub_indikator"] ?? '',
+                                    style: TextStyle(fontSize: 14, color: Colors.grey),
                                   ),
                                 ],
                               ),
-                              SizedBox(height: 4),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              controller: sebelumControllers[index],
-                              decoration: InputDecoration(
-                                labelText: 'Sebelum',
-                                border: OutlineInputBorder(),
-                                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            ),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.visibility,
+                                        color: Colors.blue,
+                                      ),
+                                      onPressed: () async {
+                                        _showPopup(context, data[index]["keterangan"] ?? '');
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.help_outline,
+                                        color: Colors.orange,
+                                      ),
+                                      onPressed: () async {
+                                        _showPopup(context, data[index]["kriteria"] ?? '');
+                                      },
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 4),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: sebelumControllers[index],
+                                decoration: InputDecoration(
+                                  labelText: 'Sebelum',
+                                  border: OutlineInputBorder(),
+                                  contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(width: 10),
-                          Expanded(
-                            child: TextField(
-                              controller: sesudahControllers[index],
-                              decoration: InputDecoration(
-                                labelText: 'Sesudah',
-                                border: OutlineInputBorder(),
-                                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: TextField(
+                                controller: sesudahControllers[index],
+                                decoration: InputDecoration(
+                                  labelText: 'Sesudah',
+                                  border: OutlineInputBorder(),
+                                  contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                ),
                               ),
                             ),
+                          ],
+                        ),
+                        SizedBox(height: 10), // Tambahkan spacing
+                        TextField(
+                          controller: keteranganControllers[index],
+                          decoration: InputDecoration(
+                            labelText: 'Keterangan',
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-          ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: _saveDataEntry,
-      child: Icon(Icons.save),
-      backgroundColor: Colors.blue,
-    ),
-  );
-}
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _saveDataEntry,
+        child: Icon(Icons.save),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
 }
